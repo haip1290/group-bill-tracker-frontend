75c4cdd13db690fa61ccc4c13422c346a4825865
Hai Pham <thanh_hai1290@yahoo.com>
2025-10-14 16:19:07 +0700

    feat: add select for status of activities on dashboard

--- src/components/DashboardPage.jsx
+++ src/components/DashboardPage.jsx
@@ -6,16 +6,19 @@ import ActivitiesList from "./ActivitiesList";
 const DashboardPage = () => {
   const { user, fetchWithAuth } = useAuthContext();
   const [activities, setActivities] = useState([]);
+  const [activitiesStatus, setActivitiesStatus] = useState("unpaid");
   const [loading, setLoading] = useState(true);
   const [message, setMessage] = useState("");
-
+  const statusOptions = ["unpaid", "achieved"];
   /**
    * @description this method fetch dashboard info from backend
    * used in useEffect hook
+   * @param {string} status of activiies (unpaid, achieved, etc...)
    */
-  const fetchUserActivities = async () => {
+  const fetchUserActivities = async (status) => {
     try {
-      const URL = "http://localhost:5000/activities";
+      console.log(`Fetching ${status} activities`);
+      const URL = `http://localhost:5000/activities?status=${status}`;
       const res = await fetchWithAuth(URL);
       const data = await res.json();
       if (!res.ok) {
@@ -23,6 +26,7 @@ const DashboardPage = () => {
         throw new Error("Faid to fetch dashboard data", data.message);
       }
       setActivities(data.data.activities);
+      console.log(`Done fetching ${status} activites`);
     } catch (error) {
       console.error("Error fetching activities ", error.message);
       setMessage("Error loading activities");
@@ -31,8 +35,13 @@ const DashboardPage = () => {
     }
   };
   useEffect(() => {
-    fetchUserActivities();
-  }, []);
+    fetchUserActivities(activitiesStatus);
+  }, [activitiesStatus]);
+
+  const onStatusSelect = (status) => {
+    console.log(`select ${status} activities`);
+    setActivitiesStatus(status);
+  };
 
   return (
     <>
@@ -41,7 +50,18 @@ const DashboardPage = () => {
         <h1>Dashboard</h1>
         <h2>Welcome {user?.email}</h2>
       </div>
-
+      <select
+        value={activitiesStatus}
+        onChange={(e) => {
+          onStatusSelect(e.target.value);
+        }}
+      >
+        {statusOptions.map((status) => (
+          <option key={status} value={status}>
+            {status}
+          </option>
+        ))}
+      </select>
       {loading && <div>Loading...</div>}
       {message && <div>Error: {message}</div>}
       {!loading && <ActivitiesList activities={activities}></ActivitiesList>}
